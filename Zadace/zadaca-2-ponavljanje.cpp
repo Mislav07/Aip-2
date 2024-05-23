f2eb11e960f0dd63f5421fed6189e3fb5a671fe4
MislavS <mislav.serdar1@gmail.com>
2024-05-23 20:45:18 +0200

    Ispravak zadace

---
+++ Zadace/zadaca-2-ponavljanje.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct Ucenik
+{
+    char imePrezime[50];
+    float prosjek;
+};
+
+bool cmp(const Ucenik &a, const Ucenik &b)
+{
+    return a.prosjek > b.prosjek;
+}
+
+int main()
+{
+    struct Ucenik ucenici[100];
+    int brUcenika = 0;
+    fstream datoteka("data.bin", ios::binary | ios ::in);
+    while (datoteka.read((char *)&ucenici[brUcenika], sizeof(Ucenik)))
+    {
+        cout << ucenici[brUcenika].imePrezime << " " << ucenici[brUcenika].prosjek << endl;
+        brUcenika++;
+    }
+    datoteka.close();
+
+    int n;
+    cout << "Unesite broj ucenika koje zelite unijeti: ";
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Unesite ime i prezime ucenika: " << endl;
+        cin.getline(ucenici[brUcenika + i].imePrezime, 50);
+
+        cout << "Unesite prosjek ucenika: " << endl;
+        cin >> ucenici[brUcenika + i].prosjek;
+        cin.ignore();
+    }
+
+    sort(ucenici, ucenici + brUcenika + n, cmp);
+
+    for (int i = 0; i < brUcenika + n; i++)
+    {
+        cout << ucenici[i].imePrezime << " " << ucenici[i].prosjek << endl;
+    }
+
+    datoteka.open("data.bin", ios::binary | ios ::out | ios::trunc);
+
+    datoteka.write((char *)ucenici, sizeof(Ucenik) * (brUcenika + n));
+    datoteka.close();
+    return 0;
+}
